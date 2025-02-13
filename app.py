da7496170bde17c8146641634e4deb55f9babd01
Reydner_matheus <85471682+reydner-matheus@users.noreply.github.com>
2025-02-12 21:08:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 print("Hello, World!")
 
-""Versão inicial do app""
+"Versão inicial do app"
+
+print("Hello, World!")
+print("Nova funcionalidade adicionada!")
+
+
+
+"Adicionando nova funcionalidade"
